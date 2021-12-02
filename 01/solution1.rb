98d39a0b6d1bda46892960ad46f3d8ca7a1b1115
Michael Hunter <tahoemph@gmail.com>
2021-12-01 18:26:42 -0800

    Day 1 part 1 completed

---
+++ 01/solution1.rb
@@ -0,0 +1,8 @@
+lines = File.read('input.txt').split("\n")
+previous = lines[0].to_i
+num_larger = 0;
+lines[1..].each do |number|
+	num_larger += 1 if number.to_i > previous
+	previous = number.to_i
+end
+puts num_larger
