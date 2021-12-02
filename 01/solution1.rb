0f77edff408f0af339962113668b4d20f62d54a8
Michael Hunter <tahoemph@gmail.com>
2021-12-01 20:14:34 -0800

    my c roots come out in the oddest of moments

--- 01/solution1.rb
+++ 01/solution1.rb
@@ -1,6 +1,6 @@
 lines = File.read('input.txt').split("\n")
 previous = lines[0].to_i
-num_larger = 0;
+num_larger = 0
 lines[1..].each do |number|
 	num_larger += 1 if number.to_i > previous
 	previous = number.to_i
