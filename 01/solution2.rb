624c145d845984acf965108450644e04dc516108
Michael Hunter <tahoemph@gmail.com>
2021-12-01 20:15:10 -0800

    copypastacroots

--- 01/solution2.rb
+++ 01/solution2.rb
@@ -1,6 +1,6 @@
 lines = File.read('input.txt').split("\n").map{|x| x.to_i}
 previous = lines[0..2].sum
-num_larger = 0;
+num_larger = 0
 (1..lines.length - 2).each do |index|
 	sum = lines[index..index+2].sum
 	num_larger += 1 if sum > previous
