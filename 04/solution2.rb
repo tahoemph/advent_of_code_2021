af7a1c51d7e85ab6ee82677beb4ab1c3f006d230
Michael Hunter <tahoemph@gmail.com>
2021-12-05 17:01:58 -0800

    day 4 complete

---
+++ 04/solution2.rb
@@ -0,0 +1,51 @@
+require 'pry'
+
+def get_board(lines)
+	board = []
+	lines.each do |line|
+		board << line.split(' ').map{|x| {used: false, value: x}}
+	end
+	board
+end
+
+def mark_value(boards, value)
+	boards.each do |board|
+		(0..4).each do | row|
+			(0..4).each do |col|
+				board[row][col][:used] = true if board[row][col][:value] == value
+			end
+		end
+	end
+end
+
+def winner?(board)
+	row_won = (0..4).map{|row| board[row].map{|x| x[:used]}.all?}.any?
+	col_won = (0..4).map{|col| board.transpose[col].map{|x| x[:used]}.all?}.any?
+	row_won || col_won
+end
+
+lines = File.read('input.txt').split("\n")
+
+input = lines[0].split(",")
+
+boards = []
+lines[2..].each_slice(6) do |lines|
+	boards << get_board(lines[0..4])
+end
+
+total_boards = boards.length
+boards_won = []
+input.each do |x|
+	mark_value(boards, x)
+	boards.each_with_index do |board, index|
+		if !boards_won.include?(index) && winner?(board)
+			boards_won << index
+			if boards_won.length == total_boards
+				unmarked = board.flatten.select{|x| !x[:used]}.map{|x| x[:value].to_i}.inject(:+)
+				puts unmarked.to_i * x.to_i
+				exit!
+			end
+		end
+	end
+end
+
