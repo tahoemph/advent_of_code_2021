9adeecd2964c598eee1c4f43b7d2e9c9f022b247
Michael Hunter <tahoemph@gmail.com>
2021-12-06 21:15:48 -0800

    day 7 complete

---
+++ 07/solution2.rb
@@ -0,0 +1,13 @@
+input = File.read('input.txt').split(',').map{|x| x.to_i}
+
+min_pos = input.min
+max_pos = input.max
+
+data = (min_pos..max_pos).map do |pos|
+	fuel_usage = input.map do |x|
+		(1..(pos - x).abs).sum
+	end.sum
+end
+
+min = data.each_with_index.min
+puts min
